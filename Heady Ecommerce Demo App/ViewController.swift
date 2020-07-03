f894e47a1c02297dd2ec51e9a60fa6231f1e23d7
Pranayj14 <pranay@globalgyan.in>
2020-07-03 21:08:40 +0530

    Heady Ecommerce Demo App

---
+++ Heady Ecommerce Demo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heady Ecommerce Demo App
+//
+//  Created by Saumya Verma on 03/07/20.
+//  Copyright © 2020 Saumya Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Heady Ecommerce Demo AppTests/Heady_Ecommerce_Demo_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Heady_Ecommerce_Demo_AppTests.swift
+//  Heady Ecommerce Demo AppTests
+//
+//  Created by Saumya Verma on 03/07/20.
+//  Copyright © 2020 Saumya Verma. All rights reserved.
+//
+
+import XCTest
+@testable import Heady_Ecommerce_Demo_App
+
+class Heady_Ecommerce_Demo_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
